f885111ad2dc6bd5f290c7a182e95d03833298d8
Maciek Jarzębski <maciek@MacBook-Air-Maciek.local>
2026-06-18 22:35:24 +0200

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,7 +28,22 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Script
+  src="https://www.googletagmanager.com/gtag/js?id=G-11EMLVM76D"
+  strategy="afterInteractive"
+/>
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-11EMLVM76D');
+  `}
+      </Script>
+{children}
+</body>
     </html>
   );
 }
